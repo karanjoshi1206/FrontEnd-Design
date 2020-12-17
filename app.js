610f9008cf6d6de1c5e5ef86092039fc4ccec301
karan1206 <64606159+karanjoshi1206@users.noreply.github.com>
2020-12-17 22:13:20 +0530

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var ai = document.getElementById("ai")
 var ml = document.getElementById("ml")
 var robo = document.getElementById("robo")
 var heading = document.getElementById("heading")
+var about = document.getElementById("about")
 function webf() {
 
     web.style.display = "grid"
@@ -12,6 +13,11 @@ function webf() {
     ml.style.display = "none"
     robo.style.display = "none"
     heading.innerText = "Web Development Projects"
+    about.innerText = ` In the modern era, we have become extremely reliable on websites, from making friends to ordering clothes to
+    entertainment. Web Development is one of the early fields of web-based technology that has not lost its
+    importance in the sands of time. Websites based companies, to maintain themselves in the run require to keep on
+    adapting to changes, making web development one of the most active fields. Web Development not only includes the
+    pages we see(frontend), but it also includes the functioning embed on those websites(backend).`
 
 }
 function appf() {
@@ -22,6 +28,7 @@ function appf() {
     ml.style.display = "none"
     robo.style.display = "none"
     heading.innerText = "App Development Projects"
+    about.innerText = `Mobile Phones have replaced most of the technologies, like mp3 players, TVs, and to some extend computers, the reason being the variety of applications and web apps that run on them. Apps are not only for mobile phones but also for computers and other such technologies. The development of apps and maintaining them is one of the most interesting fields that one can come across. Working with the User Interface to developing the app for public use is one of the most entertaining journeys.`
 
 
 }
@@ -32,7 +39,7 @@ function aif() {
     ml.style.display = "none"
     robo.style.display = "none"
     heading.innerText = "Artificial Intelligience Projects"
-
+    about.innerText = `From seeing JARVIS in movies to creating one as your personal assistant. Artificial Intelligence(AI) has expanded its range from being a test subject for chess competitors to replacing psychologists in real life. AI refers to the simulation of human actions by machines. These actions include things like learning from past actions and making new decisions based on experience. AI is a field still under development and hence has a lot of working options. Modern offices are automated offices, which is one of the applications for AI.`
 
 }
 function robof() {
@@ -46,7 +53,7 @@ function robof() {
     robo.style.display = "grid"
     heading.innerText = "Robotics Projects"
 
-
+    about.innerText = `Humans have been creating machines to do their work. Robotics is one of the applications of these fields. It involves the conception, design, manufacture, and operation of robots. Robotics is creating machines that can perform human actions such as walking, running, speaking, etc. Robotics has various applications in itself that reveal when one starts to work in this field.`
 }
 function mlf() {
 
@@ -58,7 +65,7 @@ function mlf() {
     ml.style.display = "grid"
     robo.style.display = "none"
     heading.innerText = "Machine Learning Projects"
-
+    about.innerText = `Machine learning is a sub-part of Artificial Intelligence. Machine learning is the field of study that gives computers the capability to learn without being explicitly programmed.`
 
 }
 
