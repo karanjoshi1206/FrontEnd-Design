ee2839a480292fa2dc094820c7274dafe2ebca9a
karan1206 <64606159+karanjoshi1206@users.noreply.github.com>
2020-12-17 20:51:06 +0530

    commit

---
+++ webprojects/app.js
@@ -0,0 +1,14 @@
+// bulb = document.getElementById("bulb")
+// like = document.getElementById("like")
+// bulb.addEventListner("click", () => {
+//     bulb.style.color = "yellow"
+// })
+
+function update() {
+    var bulb = document.getElementById("bulb")
+    var like = document.getElementById("like")
+    bulb.classList.toggle("toggle")
+    // var likeprev = like.innerText
+    // likec = parseInt(likeprev)
+    // like.innerText = likec + 1
+}
